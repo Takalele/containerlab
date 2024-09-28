f31f87609114f6b051a43f906b3aa9904d0567dd
Takalele <takalele@konnex.me>
2024-09-28 20:47:33 +0200

    fixes ipv6 oob address assignment
    Unfortunately, globally disabling IPv6 at init (cfg.Sysctls["net.ipv6.conf.all.disable_ipv6"] = "1") breaks IPv6 on the OOB management interface, which prevents Docker from assigning an IPv6 address to eth0. As a result, hostname -i is also unable to retrieve the IPv6 management address. This commit disables IPv6 for each interface at runtime via os/exec. This ensures that Docker can assign the IPv6 address, and hostname -i is again able to display the IPv6 address, allowing the rare init scripts to configure the IPv6 OOB interface in FreeRTR.

--- nodes/rare/rare.go
+++ nodes/rare/rare.go
@@ -7,8 +7,10 @@ package rare
 import (
 	"context"
 	"fmt"
+	"os/exec"
 	"path/filepath"
-
+	"time"
+	log "github.com/sirupsen/logrus"
 	"github.com/srl-labs/containerlab/nodes"
 	"github.com/srl-labs/containerlab/types"
 	"github.com/srl-labs/containerlab/utils"
@@ -35,32 +37,88 @@ func (n *rare) Init(cfg *types.NodeConfig, opts ...nodes.NodeOption) error {
 	for _, o := range opts {
 		o(n)
 	}
-
-	// make ipv6 disabled on all rare node interfaces unconditionally
-	// as ipv6 will be handled by rare/freertr
-	cfg.Sysctls["net.ipv6.conf.all.disable_ipv6"] = "1"
-
-	n.Cfg.Binds = append(n.Cfg.Binds,
-		fmt.Sprint(filepath.Join(n.Cfg.LabDir, "run"), ":/rtr/run"),
-	)
+	
+	n.Cfg.Binds = append(n.Cfg.Binds, fmt.Sprint(filepath.Join(n.Cfg.LabDir, "run"), ":/rtr/run"))
 
 	return nil
 }
 
-func (n *rare) PreDeploy(_ context.Context, params *nodes.PreDeployParams) error {
+func (n *rare) genInterfacesEnv() {
+	// Add eth0 manually
+	n.Cfg.Env["CLAB_INTF_0"] = "eth0"
+
+	for i, e := range n.Endpoints {
+		ifaceName := e.GetIfaceName()
+		envKey := fmt.Sprintf("CLAB_INTF_%d", i+1)
+		n.Cfg.Env[envKey] = ifaceName
+	}
+}
+
+
+func (n *rare) PreDeploy(ctx context.Context, params *nodes.PreDeployParams) error {
+	// Generate the interface environment variables
+	n.genInterfacesEnv()
+
 	utils.CreateDirectory(n.Cfg.LabDir, 0777)
+
 	_, err := n.LoadOrGenerateCertificate(params.Cert, params.TopologyName)
 	if err != nil {
-		return nil
+		return err
 	}
 
 	return n.createRAREFiles()
 }
 
+func (n *rare) PostDeploy(ctx context.Context, params *nodes.PostDeployParams) error {
+	// disable IPv6 at runtime for every interface and globally
+	// Retry loop to wait until the container is fully running
+	for {
+		// Check if the container is running
+		cmd := exec.Command("docker", "inspect", "-f", "{{.State.Running}}", n.Cfg.LongName)
+		output, err := cmd.CombinedOutput()
+		if err != nil || string(output) != "true\n" {
+			log.Infof("Container %s not yet running, waiting...\n", n.Cfg.LongName)
+			time.Sleep(2 * time.Second) // Wait a bit and retry
+		} else {
+			break // Container is running, exit the loop
+		}
+	}
+
+	// Proceed with sysctl commands for individual interfaces if container exists
+	for i := 0; ; i++ {
+		envKey := fmt.Sprintf("CLAB_INTF_%d", i)
+		if iface, ok := n.Cfg.Env[envKey]; ok {
+			// Use os/exec to set the sysctl values after container start
+			cmd := exec.Command("docker", "exec", n.Cfg.LongName, "sysctl", "-w", fmt.Sprintf("net.ipv6.conf.%s.disable_ipv6=1", iface))
+			_, err := cmd.CombinedOutput()
+			if err != nil {
+				return fmt.Errorf("Failed to disable IPv6 on interface %s of container %s", iface, n.Cfg.LongName)
+			}
+		} else {
+			break // No more interfaces in the environment variables
+		}
+	}
+
+	// Disable IPv6 globally for all and default
+	sysctlCommands := []string{
+		"net.ipv6.conf.all.disable_ipv6=1",
+		"net.ipv6.conf.default.disable_ipv6=1",
+	}
+
+	for _, sysctlCmd := range sysctlCommands {
+		cmd := exec.Command("docker", "exec", n.Cfg.LongName, "sysctl", "-w", sysctlCmd)
+		_, err := cmd.CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("Failed to execute sysctl command %s on container %s", sysctlCmd, n.Cfg.LongName)
+		}
+	}
+
+	return nil
+}
+
 func (n *rare) createRAREFiles() error {
-	nodeCfg := n.Config()
-	// create "run" directory that will be bind mounted to rare node
-	utils.CreateDirectory(filepath.Join(nodeCfg.LabDir, "run"), 0777)
+	// Create the "run" directory that will be bind mounted to the rare node
+	utils.CreateDirectory(filepath.Join(n.Cfg.LabDir, "run"), 0777)
 
 	return nil
 }
